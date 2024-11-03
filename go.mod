b43afacb95a5cb404c74d39efa15b4f7a89e40e2
james <james@jmcdowall.com>
2024-11-03 23:49:07 +0000

    Added and Configured Viper For Command Line Params

--- go.mod
+++ go.mod
@@ -40,29 +40,35 @@ require (
 	github.com/ganigeorgiev/fexpr v0.4.1 // indirect
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.4+incompatible // indirect
-	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/pprof v0.0.0-20240827171923-fa2c70bbbfe5 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/labstack/echo/v5 v5.0.0-20230722203903-ec5b858dab61 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pocketbase/dbx v1.10.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.7.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/spf13/viper v1.19.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	gocloud.dev v0.40.0 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
@@ -73,12 +79,13 @@ require (
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/term v0.25.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
-	golang.org/x/time v0.7.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
 	google.golang.org/api v0.201.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
 	google.golang.org/grpc v1.67.1 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	modernc.org/gc/v3 v3.0.0-20241004144649-1aea3fae8852 // indirect
 	modernc.org/libc v1.61.0 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
--- main.go
+++ main.go
@@ -10,76 +10,52 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
-	// "github.com/pocketbase/pocketbase/plugins/ghupdate"
 	"github.com/pocketbase/pocketbase/plugins/jsvm"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 	"github.com/pocketbase/pocketbase/tools/hook"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	app := pocketbase.New()
 
 	// ---------------------------------------------------------------
-	// Optional plugin flags:
+	// Load configuration using Viper:
 	// ---------------------------------------------------------------
 
-	var hooksDir string
-	app.RootCmd.PersistentFlags().StringVar(
-		&hooksDir,
-		"hooksDir",
-		"",
-		"the directory with the JS app hooks",
-	)
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	viper.AutomaticEnv()          // read in environment variables that match
 
-	var hooksWatch bool
-	app.RootCmd.PersistentFlags().BoolVar(
-		&hooksWatch,
-		"hooksWatch",
-		true,
-		"auto restart the app on pb_hooks file change",
-	)
+	// Check for config file in multiple formats
+	configFiles := []string{"config.toml", "config.yaml", "config.json"}
+	for _, configFile := range configFiles {
+		viper.SetConfigFile(configFile)
+		if err := viper.ReadInConfig(); err == nil {
+			break
+		}
+	}
 
-	var hooksPool int
-	app.RootCmd.PersistentFlags().IntVar(
-		&hooksPool,
-		"hooksPool",
-		15,
-		"the total prewarm goja.Runtime instances for the JS app hooks execution",
-	)
+	// Set default values
+	viper.SetDefault("hooksDir", "")
+	viper.SetDefault("hooksWatch", true)
+	viper.SetDefault("hooksPool", 15)
+	viper.SetDefault("migrationsDir", "")
+	viper.SetDefault("automigrate", true)
+	viper.SetDefault("publicDir", defaultPublicDir())
+	viper.SetDefault("indexFallback", true)
 
-	var migrationsDir string
-	app.RootCmd.PersistentFlags().StringVar(
-		&migrationsDir,
-		"migrationsDir",
-		"",
-		"the directory with the user defined migrations",
-	)
-
-	var automigrate bool
-	app.RootCmd.PersistentFlags().BoolVar(
-		&automigrate,
-		"automigrate",
-		true,
-		"enable/disable auto migrations",
-	)
-
-	var publicDir string
-	app.RootCmd.PersistentFlags().StringVar(
-		&publicDir,
-		"publicDir",
-		defaultPublicDir(),
-		"the directory to serve static files",
-	)
-
-	var indexFallback bool
-	app.RootCmd.PersistentFlags().BoolVar(
-		&indexFallback,
-		"indexFallback",
-		true,
-		"fallback the request to index.html on missing static path (eg. when pretty urls are used with SPA)",
-	)
-
-	app.RootCmd.ParseFlags(os.Args[1:])
+	// Bind command line flags
+	pflag.String("hooksDir", "", "the directory with the JS app hooks")
+	pflag.Bool("hooksWatch", true, "auto restart the app on pb_hooks file change")
+	pflag.Int("hooksPool", 15, "the total prewarm goja.Runtime instances for the JS app hooks execution")
+	pflag.String("migrationsDir", "", "the directory with the user defined migrations")
+	pflag.Bool("automigrate", true, "enable/disable auto migrations")
+	pflag.String("publicDir", defaultPublicDir(), "the directory to serve static files")
+	pflag.Bool("indexFallback", true, "fallback the request to index.html on missing static path (eg. when pretty urls are used with SPA)")
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
 
 	// ---------------------------------------------------------------
 	// Plugins and hooks:
@@ -87,26 +63,25 @@ func main() {
 
 	// load jsvm (pb_hooks and pb_migrations)
 	jsvm.MustRegister(app, jsvm.Config{
-		MigrationsDir: migrationsDir,
-		HooksDir:      hooksDir,
-		HooksWatch:    hooksWatch,
-		HooksPoolSize: hooksPool,
+		MigrationsDir: viper.GetString("migrationsDir"),
+		HooksDir:      viper.GetString("hooksDir"),
+		HooksWatch:    viper.GetBool("hooksWatch"),
+		HooksPoolSize: viper.GetInt("hooksPool"),
 	})
 
 	// migrate command (with js templates)
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
 		TemplateLang: migratecmd.TemplateLangJS,
-		Automigrate:  automigrate,
-		Dir:          migrationsDir,
+		Automigrate:  viper.GetBool("automigrate"),
+		Dir:          viper.GetString("migrationsDir"),
 	})
-
-	// // GitHub selfupdate
-	// ghupdate.MustRegister(app, app.RootCmd, ghupdate.Config{})
 
 	// static route to serves files from the provided public dir
 	// (if publicDir exists and the route path is not already defined)
 	app.OnServe().Bind(&hook.Handler[*core.ServeEvent]{
 		Func: func(e *core.ServeEvent) error {
+			publicDir := viper.GetString("publicDir")
+			indexFallback := viper.GetBool("indexFallback")
 			if !e.Router.HasRoute(http.MethodGet, "/{path...}") {
 				e.Router.GET("/{path...}", apis.Static(os.DirFS(publicDir), indexFallback))
 			}
